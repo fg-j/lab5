d0239fb41a19aa9b117d93f6ce8c4970169d8e65
hmc-cs-sfriedrich <sfriedrich@hmc.edu>
2017-11-09 22:51:52 -0800

    finished the lab

--- ringbuf.c
+++ ringbuf.c
@@ -1,3 +1,7 @@
+/********************************************/
+/* FLORA GALLINA-JONES AND SASHA FRIEDRICH  */
+/********************************************/
+
 #include <time.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -27,8 +31,7 @@ int nextToEmpty = 0;
 pthread_mutex_t mutex;
 
 // Create buffer conditions
-pthread_cond_t buffFull; 
-pthread_cond_t buffEmpty;
+pthread_cond_t buffCheck; 
 
 // declare timespec struct to use for sleeping
 struct timespec sleepTime;
@@ -99,92 +102,107 @@ void Pthread_cond_signal(pthread_cond_t* cond){
     unix_error("Condition signal error");
   }
 }
-/*
-void Pthread_exit(void* return){
 
-    pthread_exit();
-  if (error != 0) {
-    unix_error("Pthread exit error");
-  }
-}
-*/
-/*
-bool Scanf() {
-  // do the scanning
-  // if read end of file, return True
-  // else, return false
-  int error = ;
-  if (error != 0) {
-    unix_error("Scanf error");
-  }
-}
-*/
 void millisleep(int sleeptime) {
   // wrapper for nanosleep
   long adjustedTime = sleeptime*(10^6);
+  long maxTime = 999999999;
+  while (adjustedTime > maxTime) {
+    sleepTime.tv_nsec = maxTime;
+    sleepTime.tv_sec = 0;
+
+    adjustedTime -= maxTime;
+
+    int err = nanosleep(&sleepTime, NULL);
+
+    if (err) {
+      unix_error("Sleep error");
+    }
+
+  }
+
   sleepTime.tv_nsec = adjustedTime;
   sleepTime.tv_sec = 0;
-
   int err = nanosleep(&sleepTime, NULL);
 
   if (err) {
     unix_error("Sleep error");
   }
-
 }
+
 /***********************/
 
 void * consumer(void * returnval) {
-  int lineNum = 0;
+  //printf("consumer called \n");
   int totalSum = 0;
   
-  // Pick up the mutex
-  Pthread_mutex_lock(&mutex);
+  while (1) {
+    // Pick up the mutex
+    Pthread_mutex_lock(&mutex);
+    // printf("consumer grabs mutex \n");
+    
+    while(bufferSize == 0) {
+      Pthread_cond_wait(&buffCheck, &mutex); // puts down the lock until receives ping from producer
+    }
+    // printf("Consumer buffer size before read: %d \n", bufferSize);
+    // We have the mutex -- read from the buffer
+    message tempMes;
+    tempMes = buffer[nextToEmpty];
+    nextToEmpty = (nextToEmpty + 1) % BUFFER_SIZE;
+    bufferSize--;
+    // printf("Consumer buffer size after read: %d \n", bufferSize); 
+    
+    //drop the mutex
+    Pthread_mutex_unlock(&mutex);
+    // printf("consumer dropped mutex \n");
 
-  while(bufferSize == 0) {
-    Pthread_cond_wait(&buffEmpty, &mutex); // puts down the lock until receives ping from producer
+    // Send a message to the producer to check if the buffer is still full
+    Pthread_cond_signal(&buffCheck);
+
+    // Check for quit message
+    if (tempMes.quit) {
+      printf("Final sum is %d\n", totalSum);
+      pthread_exit(NULL);
+    }
+    
+    millisleep(tempMes.consumer_sleep);
+    totalSum += tempMes.value; // add value from buffer to running total
+    if (tempMes.print_code == 2 || tempMes.print_code == 3) {
+      printf("Consumed %d from input line %d; sum = %d\n", tempMes.value, tempMes.line, totalSum);
+    }
   }
-  // We have the mutex -- read from the buffer
-  message tempMes;
-  tempMes = buffer[nextToEmpty];
-  nextToEmpty = (nextToEmpty + 1) % BUFFER_SIZE;
-  bufferSize--; 
-  
-  //drop the mutex
-  Pthread_mutex_unlock(&mutex);
-
-  // Send a message to the producer to check if the buffer is still full
-  Pthread_cond_signal(&buffFull);
-
-  // Check for quit message
-  if (tempMes.quit) {
-    printf("Final sum is %d\n", totalSum);
-    pthread_exit(NULL);
-  }
-  
-  millisleep(tempMes.consumer_sleep);
-  totalSum += tempMes.value; // add value from buffer to running total
-  if (tempMes.print_code == 2 || tempMes.print_code == 3) {
-    printf("Consumed %d from input line %d; sum = %d\n", tempMes.value, tempMes.line, totalSum);
-  }
-
-  pthread_exit(NULL);
 }
 
 void producer() {
+  // printf("producer called \n");
    
   int lineNum = 1;  
   int eof = 0;
 
   while (eof != EOF){
+    // printf("producer entered scanf loop \n");
     message  mes;
     // Read a line from stdinput
 
     int producer_sleep;
     mes.line = lineNum;
 
-    eof = scanf("%d %d %d %d", &(mes.value), &(producer_sleep), &(mes.consumer_sleep), &(mes.print_code));
+    int tempVal;
+    int tempConSleep;
+    int tempPrintCode;
+    // mes.value = 1;
+    // producer_sleep = 1;
+    // mes.consumer_sleep = 1;
+    // mes.print_code = 3;
+
+    //eof = scanf("%d", &(mes.value));
+    eof = scanf("%d %d %d %d", &tempVal, &producer_sleep, &tempConSleep, &tempPrintCode);
+    // printf("successful scanf \n");
     lineNum++;
+
+    mes.value = tempVal;
+    mes.consumer_sleep = tempConSleep;
+    mes.print_code = tempPrintCode;
     // Reading/parse a line from stdin
 
     if (eof == EOF) {
@@ -193,24 +211,29 @@ void producer() {
     else {
       mes.quit = 0;
       millisleep(producer_sleep);
+      // printf("producer finished sleep \n");
     }
 
     // pick up the mutex
     Pthread_mutex_lock(&mutex);
+    // printf("producer grabs mutex \n");
     // Only want to write to buffer if it's not full
     while (bufferSize == BUFFER_SIZE){ // no room in the buffer
-      Pthread_cond_wait(&buffFull, &mutex); // briefly puts down the mutex
+      Pthread_cond_wait(&buffCheck, &mutex); // briefly puts down the mutex
     }
+    // printf("Producer buffer size before add: %d \n", bufferSize);
     // We have the mutex -- write to the buffer
     
     buffer[nextToFill] = mes;
     nextToFill = (nextToFill+1) % BUFFER_SIZE;
     bufferSize++;
+    // printf("Producer buffer size after add: %d \n", bufferSize);
 
     // Drop the mutex
     Pthread_mutex_unlock(&mutex);
+    // printf("producer dropped mutex \n");
 
-    Pthread_cond_signal(&buffEmpty); // signal to consumer thread to check status of buffer again
+    Pthread_cond_signal(&buffCheck); // signal to consumer thread to check status of buffer again
     
     if (mes.print_code == 1 || mes.print_code == 3) {
       printf("Produced %d from input line %d\n", mes.value, mes.line);
@@ -225,19 +248,22 @@ int main() {
 
   setlinebuf(stdout);
 
-  //Initialize the mutex 
+  // printf("Started run \n");
+  //Initialize the mutex
   Pthread_mutex_init(&mutex, NULL);
+  // printf("Initialized mutex \n");
   
   //Initialize the buffer conditions
-  Pthread_cond_init(&buffFull, NULL);
-  Pthread_cond_init(&buffEmpty, NULL);
+  Pthread_cond_init(&buffCheck, NULL);
+  // printf("condition init\n");
 
 
   //Create the consumer thread
   pthread_t consumer_thread;
   Pthread_create(&consumer_thread, NULL, &consumer, NULL); //Inputs to consumer?
+  // printf("thread created\n");
 
-  producer(&mutex, &buffFull, &buffEmpty);
+  producer();
 
   Pthread_join(consumer_thread, NULL);
 
